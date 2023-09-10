67245d0508da164f1d435b7b323641dcdfc9ee30
spacecaseartist <josh.schmitz.dev@gmail.com>
2023-09-10 00:29:14 -0500

    formatting and comment changes for part10

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-// no analogous main function is JS
+// no main function is JS - changed comment for part 10
 // user input prompt show up in browser immediately
-let input = prompt(
- "Enter a list of numbers, separated by commas, to create our actionable array:"
-);
+let input = prompt("Enter a list of numbers, separated by commas, to create our actionable array:");
+
+//convert string to numbers
 let arra = input.split(",").map(Number);
 
 //function to calc sum of elements in an array
 function sum(arra) {
- let s = 0;
 
+ let s = 0;
  for (let i = 0; i < arra.length; i++) s += arra[i];
  return s;
+
 }
 
 //function to calc product of elements in an array
 function product(arra) {
+
  let prod = 1;
  for (i = 0; i < arra.length; i++) prod = prod * arra[i];
  return prod;
+
 }
 
 let arrarev = arra;
