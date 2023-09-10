15f7ec424b2403e72038d910cbb70ddcb33c387a
spacecaseartist <josh.schmitz.dev@gmail.com>
2023-09-10 00:13:10 -0500

    adds array product function

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//main.js
-
 //function to calc sum of elements in an array
 function sum(arra) { 
     let s = 0;
@@ -9,6 +7,15 @@ function sum(arra) {
     return s; 
 } 
  
+//function to calc product of elements in an array
+function product(arra) {
+    let prod = 1;
+    for (i = 0; i < arra.length; i++)
+        prod = prod * arra[i];
+    return prod;
+}
+
 let arra = [1, 8, 16, 24];
 document.write("Array is " + arra + "<br>");
 document.write("Array Sum is " + sum(arra) + "<br>");
+document.write("Array product is " + product(arra) + "<br>");
